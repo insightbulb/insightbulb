5d0822af5df1f717caf0bc3736457fb2a6f0d1a0
Wyatt Hoodes <hoodesw@hawaii.edu>
2019-03-18 22:46:47 -1000

    web_scraping: Station data is now sent to the front end.
    There are still major styling issues that need to be dealt with in terms
    of a secondary dropdown opening on clicking a region.
    
    The next step is sending the station number chosen to the backend to
    scrape the tide data incrementally.

--- tide_scraper.py
+++ tide_scraper.py
@@ -12,11 +12,31 @@ def get_regions():
     return regions
 
 
-def get_stations():
+def get_stations_dict():
     soup = BeautifulSoup(response, 'html.parser')
-    stations = soup.findAll("div", attrs={'class': 'span4 station'})
+    stations_dict = dict()
+    regions = soup.findAll("div", attrs={'class': 'span12 areaheader'})
+    keys = []
 
-    # For checking the values...
-    # for station in stations:
-    #      print(station.text)
-    return stations
+    for region in regions:
+        keys.append(region.get('id'))
+    for key in keys:
+        stations_dict[key] = []
+    for key in stations_dict:
+        for region in regions:
+            if key in region.get('id'):
+                temp = []
+                sub_regions = region.findAll("div", attrs={'class': lambda l: l and l.startswith('span4')})
+                for sub_region in sub_regions:
+                    value = "%s" % sub_region.find('a').text
+                    temp.append(value)
+            stations_dict[key] = temp
+    return stations_dict
+
+
+# Might want to consider implementing this cleaner approach
+def get_station_by_region(region):
+    stations_dict = get_stations_dict()
+    local_stations = stations_dict.get(region)
+    print(local_stations)
+    return local_stations

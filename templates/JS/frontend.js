70957ee7ac786b1ca42f5bb9b2c630f0e33a825b
Christopher Lau <christopherjnlau@gmail.com>
2019-04-29 13:40:10 -1000

    added wyatt's functions

--- templates/JS/frontend.js
+++ templates/JS/frontend.js
@@ -1,4 +1,18 @@
 $(function () {
+  $('#change-graph-waves').on('click', function () {
+    $("#lineChart").hide();
+    $("#waveChart").show();
+  })
+});
+
+$(function () {
+  $('#change-graph-tides').on('click', function () {
+    $("#lineChart").show();
+    $("#waveChart").hide();
+  })
+});
+
+$(function () {
     $('#help-modal').on('click', function () {
         $('#welcome-modal').modal('show');
     });
@@ -72,7 +86,7 @@ $(function () {
             data: JSON.stringify({station_data: station_data}),
             dataType: "json"
         });
-
+        $("#waveChart").hide();
         $('#station-loader').show();
         setTimeout(function () {
             $('#station-loader').hide();
@@ -142,4 +156,44 @@ $(function () {
             }
         });
     });
+});
+
+$(function () {
+  var ctx = document.getElementById('waveChart').getContext('2d');
+  var waveChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+      labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+      datasets: [{
+        label: '# of Votes',
+        data: [12, 19, 3, 5, 2, 3],
+        backgroundColor: [
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(54, 162, 235, 0.2)',
+          'rgba(255, 206, 86, 0.2)',
+          'rgba(75, 192, 192, 0.2)',
+          'rgba(153, 102, 255, 0.2)',
+          'rgba(255, 159, 64, 0.2)'
+        ],
+        borderColor: [
+          'rgba(255, 99, 132, 1)',
+          'rgba(54, 162, 235, 1)',
+          'rgba(255, 206, 86, 1)',
+          'rgba(75, 192, 192, 1)',
+          'rgba(153, 102, 255, 1)',
+          'rgba(255, 159, 64, 1)'
+        ],
+        borderWidth: 1
+      }]
+    },
+    options: {
+      scales: {
+        yAxes: [{
+          ticks: {
+            beginAtZero: true
+          }
+        }]
+      }
+    }
+  });
 });
